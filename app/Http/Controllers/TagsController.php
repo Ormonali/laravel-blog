f9c1eec8f759f7d3e130a9cafcc5b5a75405e4fa
ormonali <kaarov8@gmail.com>
2019-06-17 07:42:31 +0600

    some changes

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -7,9 +7,5 @@ use App\Tag;
 
 class TagsController extends Controller
 {
-    public function index(Tag $tag){
-      $posts =$tag->post;
-      $posts = order_by()
-      return view('posts.index',compact('posts'));
-    }
+
 }
